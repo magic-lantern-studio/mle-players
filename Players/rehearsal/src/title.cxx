9c142ac2ef4fb4d176aa95d76cba6a0f4b4aceb1
Mark Millard <msm@wizzerworks.com>
2024-04-05 17:39:24 -0600

    Cleaning up autoconf build and source code warnings.

--- Players/rehearsal/src/title.cxx
+++ Players/rehearsal/src/title.cxx
@@ -12,7 +12,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2003-2021 Wizzer Works
+// Copyright (c) 2003-2024 Wizzer Works
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -148,9 +148,12 @@ InitEnv(int argc,char **argv)
     if (dbxenv && strstr(dbxenv,argv[0]))
 	{
 		char cmd[128];
+
         // Todo: Find a replacement for winterm and dbx.
 		sprintf(cmd,"winterm -c dbx -p %d",getpid());
-		system(cmd);
+		int status = system(cmd);
+		// Todo: Report status here; if failed, then return MlFalse.
+
 		sleep(20);
 	}
 #endif /* __linux__ */
